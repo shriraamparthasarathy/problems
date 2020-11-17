7b9a0c325f20c0277357705021c99c27b7adc4ca
shriraamparthasarathy <31043119+shriraamparthasarathy@users.noreply.github.com>
2020-11-17 23:07:27 +0530

    Update SetMatrix0or1.java

--- arrayMatrix/SetMatrix0or1.java
+++ arrayMatrix/SetMatrix0or1.java
@@ -45,9 +45,9 @@ class SetMatrix0or1
        	 	System.out.println("The changed array:");
         	for(int i=0;i<a.length;i++)
         	{
-            	for(int j=0;j<a[0].length;j++)
-                	System.out.print(a[i][j]+" ");
-            	System.out.println();    
+            		for(int j=0;j<a[0].length;j++)
+                		System.out.print(a[i][j]+" ");
+            		System.out.println();    
         	}
 	}
 }
