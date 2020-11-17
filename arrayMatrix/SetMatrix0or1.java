539f4e2081b38a4523be75aa49b4b242618209a8
shriraamparthasarathy <31043119+shriraamparthasarathy@users.noreply.github.com>
2020-11-17 23:06:55 +0530

    Update SetMatrix0or1.java

--- arrayMatrix/SetMatrix0or1.java
+++ arrayMatrix/SetMatrix0or1.java
@@ -41,13 +41,13 @@ class SetMatrix0or1
 		int a[][] = {{1, 0, 0, 1}, 
                 {0, 0, 1, 0}, 
                 {0, 0, 0, 0}}; 
-        setMatrix(a);        
-        System.out.println("The changed array:");
-        for(int i=0;i<a.length;i++)
-        {
-            for(int j=0;j<a[0].length;j++)
-                System.out.print(a[i][j]+" ");
-            System.out.println();    
-        }
+        	setMatrix(a);        
+       	 	System.out.println("The changed array:");
+        	for(int i=0;i<a.length;i++)
+        	{
+            	for(int j=0;j<a[0].length;j++)
+                	System.out.print(a[i][j]+" ");
+            	System.out.println();    
+        	}
 	}
 }
